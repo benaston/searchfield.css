77f6409eabb2a2db3970a87417e8fc6b25f29ba6
Ben Aston <ben.aston@rbi.co.uk>
2013-02-07 22:53:57 +0000

    foo

--- page.js
+++ page.js
@@ -7,4 +7,6 @@ jQuery(document).ready(function () {
 	$(".navbar a:nth-child(1)").bind('click', function(){
 		$('.three-d-context').addClass('flipped');
 	});
+	
+	$('.back-to-top').bind('click', function(){$('.result-details').scrollTop(0); return false;});
 }); 
