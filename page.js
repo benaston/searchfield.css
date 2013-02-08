130fc28d68a0c26f9f7afdfc5e2b4d8f51fb3c26
Ben Aston <ben.aston@rbi.co.uk>
2013-02-08 13:47:44 +0000

    foo

--- page.js
+++ page.js
@@ -16,4 +16,24 @@ jQuery(document).ready(function () {
 	$('body').bind('ontouchmove', function(event){
     event.preventDefault();
 	});
+	
+	
+
 }); 
+
+function allowDrop(ev)
+	{
+	ev.preventDefault();
+	}
+
+	function drag(ev)
+	{
+	ev.dataTransfer.setData("Text",ev.target.id);
+	}
+
+	function drop(ev)
+	{
+	ev.preventDefault();
+	var data=ev.dataTransfer.getData("Text");
+	$('.favourites').append(document.getElementById(data));
+	}
