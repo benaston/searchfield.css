3a0778d88d7d5a5e7dd8fffc94922a836636f843
Ben Aston <ben.aston@rbi.co.uk>
2013-02-08 00:07:29 +0000

    foo

--- page.js
+++ page.js
@@ -1,8 +1,11 @@
+var myScroll;
 jQuery(document).ready(function () {
     //used to unify font line height appearance Mac/PC
 	if (navigator.userAgent.indexOf('Mac') > 0) {
 		$("body").addClass("mac");
 	}
+	
+	myScroll = new iScroll('wrapper');
 	
 	$(".navbar a:nth-child(1)").bind('click', function(){
 		$('.three-d-context').addClass('flipped');
