f3995944b3c280d81cbde36fad1490c0bb79738b
Ben Aston <ben.aston@rbi.co.uk>
2013-02-09 23:55:27 +0000

    foo

--- page.js
+++ page.js
@@ -1,14 +1,36 @@
+//NOTE: This is just an experiment. I will transition to client-side MVC if this evolves into something more.
 var myScroll;
 jQuery(document).ready(function () {
     //used to unify font line height appearance Mac/PC
 	if (navigator.userAgent.indexOf('Mac') > 0) {
 		$("body").addClass("mac");
 	}
+		
+	$(".btn-my-account").bind('click', function(){
+		$('.app-wrapper.three-d-context').addClass('flipped');
+	});
 	
+	$(".btn-back-to-search").bind('click', function(){
+		$('.app-wrapper.three-d-context').removeClass('flipped');
+	});
 	
+	$("#btn-it-contractor,#btn-employer").bind('click', function(){	
+		$(".welcome-panel").addClass('hidden');
+		$('.search-panel, .results , .result-details, .navbar').removeClass('hidden');
+	});
 	
-	$(".navbar a:nth-child(1)").bind('click', function(){
-		$('.three-d-context').addClass('flipped');
+	$(".btn-log-out").bind('click', function(){			
+		$('.app-wrapper.three-d-context').removeClass('flipped');
+		$('.search-panel, .results , .result-details, .navbar').addClass('hidden');
+		$(".welcome-panel").removeClass('hidden');
+	});
+	
+	$(".welcome").bind('click', function(){
+		$('.welcome-panel').addClass('flipped');
+	});
+	
+	$(".back-to-welcome").bind('click', function(){
+		$('.welcome-panel').removeClass('flipped');
 	});
 	
 	$('.back-to-top').bind('click', function(){$('.result-details').scrollTop(0); return false;});
@@ -16,8 +38,6 @@ jQuery(document).ready(function () {
 	$('body').bind('ontouchmove', function(event){
     event.preventDefault();
 	});
-	
-	
 
 }); 
 
